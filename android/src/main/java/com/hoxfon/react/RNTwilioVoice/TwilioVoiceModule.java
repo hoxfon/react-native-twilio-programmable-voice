82f94ff44de9e45aa6c455605521ab91c50a4ac3
eabait <estebanabait@gmail.com>
2017-08-09 12:54:04 -0300

    Disables notification actions in android

--- android/src/main/java/com/hoxfon/react/RNTwilioVoice/TwilioVoiceModule.java
+++ android/src/main/java/com/hoxfon/react/RNTwilioVoice/TwilioVoiceModule.java
@@ -327,7 +327,9 @@ public class TwilioVoiceModule extends ReactContextBaseJavaModule implements Act
                 // Dismiss the notification when the user tap on the relative notification action
                 // eventually the notification will be cleared anyway
                 // but in this way there is no UI lag
-                notificationManager.cancel(intent.getIntExtra(INCOMING_CALL_NOTIFICATION_ID, 0));
+                // notificationManager.cancel(intent.getIntExtra(INCOMING_CALL_NOTIFICATION_ID, 0));
+
+                notificationManager.cancelAll();
             }
         }, intentFilter);
     }
