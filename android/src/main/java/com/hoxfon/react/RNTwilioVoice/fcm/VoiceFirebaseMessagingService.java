7569487be5bc43fa2f84e1b5701684581ac53217
Eelco Wiersma <contact@pagebakers.nl>
2019-09-27 15:06:05 +0200

    CANCELLED_CALL_INVITE

--- android/src/main/java/com/hoxfon/react/RNTwilioVoice/fcm/VoiceFirebaseMessagingService.java
+++ android/src/main/java/com/hoxfon/react/RNTwilioVoice/fcm/VoiceFirebaseMessagingService.java
@@ -134,7 +134,7 @@ public class VoiceFirebaseMessagingService extends FirebaseMessagingService {
             });
 
             if (!valid) {
-                Log.e(TAG, "Error handling FCM message" + messageException.toString());
+                Log.e(TAG, "Error handling FCM message");
             }
         }
 
