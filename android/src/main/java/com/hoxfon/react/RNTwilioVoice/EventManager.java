22f426791c4bcfc3e5d8044b0effcaf474905cf8
Fabrizio Moscon <mosconfabrizio@gmail.com>
2019-12-04 21:07:35 +0000

    Android X migration

--- android/src/main/java/com/hoxfon/react/RNTwilioVoice/EventManager.java
+++ android/src/main/java/com/hoxfon/react/RNTwilioVoice/EventManager.java
@@ -1,6 +1,6 @@
 package com.hoxfon.react.RNTwilioVoice;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
